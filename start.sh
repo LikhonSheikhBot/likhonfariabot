4f80bf983016979e76d82c2d8e167369e98a3838
Likhon Sheikh <93394527+LikhonSheikhBot@users.noreply.github.com>
2021-11-09 22:52:54 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/LikhonSheikhBot/likhonfariabot.git /likhonfariabot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /likhonfariabot
 fi
-cd /EvaMaria
+cd /likhonfariabot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
